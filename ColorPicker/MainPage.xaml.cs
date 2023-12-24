9c0cb633679058fe78c4ae665d4cb8f2eb849ea5
truong lam <truonglamthientai321@gmail.com>
2023-12-25 01:26:22 +0300

    .

---
+++ ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ColorPicker;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+        InitializeComponent();
+	}
+	void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+	{
+		double _red = RedSlider.Value;
+		double _green = GreenSlider.Value;
+		double _blue = BlueSlider.Value;
+		int red = (int)(_red * 255);
+		int green = (int)(_green * 255);
+		int blue = (int)(_blue * 255);
+
+		string hex = $"{red:X2}{green:X2}{blue:X2}";
+		ColorGenerate.Text = hex;
+		ColorImage.BackgroundColor = Color.FromHex(hex);
+		Main.BackgroundColor = Color.FromHex(hex);
+		Console.WriteLine($"{red}");
+	}
+}
+
+
---
+++ ColorPicker/readme.md
@@ -0,0 +1,1 @@
+This is the first project while learning MAUI. It is a simple app that let user choose color base on the code of red, green, blue.
